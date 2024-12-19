657cd92e3507433379686d53070d4792cd4d3fef
park sang woo <psw7205@gmail.com>
2024-12-19 10:28:53 +0900

    fix: typo

--- app/person/constants.ts
+++ app/person/constants.ts
@@ -1,4 +1,4 @@
-export const API_URL = process.env.API_URL || "";
+export const PERSON_API_URL = process.env.PERSON_API_URL || "";
 
 export interface PersonProps {
   id: string;
--- app/person/page.tsx
+++ app/person/page.tsx
@@ -1,9 +1,9 @@
-import { API_URL, PersonProps } from "@/app/person/constants";
+import { PERSON_API_URL, PersonProps } from "@/app/person/constants";
 import PersonCard from "@/components/person-card";
 import styles from "@/styles/page.module.css";
 
 export default async function PersonListPage() {
-  const res = await fetch(API_URL);
+  const res = await fetch(PERSON_API_URL);
   const billions: PersonProps[] = await res.json();
 
   return (
